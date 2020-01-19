015656066be736452c6b39c16ab21f588bd233e6
risonha2000 <56550927+risonha2000@users.noreply.github.com>
2020-01-19 23:25:42 +0000

    Add files via upload

---
+++ fichas_praticas/ficha3/ex4.php
@@ -0,0 +1,46 @@
+<?php
+	/******** DESCRIÇÃO DO DOCUMENTO ********
+	* Nome do ficheiro: ex4.php 			*
+	* UC: Programação II 					*
+	* @author Ema Guedes					*
+	* @version 1.0 							*
+	* Data: Janeiro de 2020 				*
+	* Descrição: ficha 3, exercício 4		*
+	*****************************************/
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Ficha 3, Exercício 4</title>
+    </head>
+    <body>
+        <h2>Números de 0 até o número que inseriu.</h2> 
+        <form name="ex5" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?> ">
+            <label for="num">Insira um valor <strong>inteiro positivo</strong>:</label>
+			<input type="number" name="num">
+			<br><br>
+			<input type="submit" value="Resultado">
+			<br><br>
+        </form>
+
+        <?php
+            if (isset($_REQUEST["num"])) {
+                $num = $_REQUEST["num"];
+
+				if ($num<0) {
+					echo "Insira um valor <strong>inteiro positivo</strong>.";
+				} else {
+                    function num($i, $num)
+					$i=0; $i <= $num ; $i++ 
+					return $i;
+					}
+				}
+            }
+        ?>
+    </body>
+</html>
